881966ff5aef27aa013767326666f0bc9cddba83
andihsu <105141169+andihsu@users.noreply.github.com>
2024-08-31 13:22:25 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 import LogSystem
 
-log = LogSystem.LogSystem(level = "INFf")
+log = LogSystem.LogSystem(level = "INFO")
 
 print(log.error('This is a message',return_message = True , write_to_file = True))
